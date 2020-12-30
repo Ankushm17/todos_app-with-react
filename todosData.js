df836f0f83e2a5041852a21061d76c38a56e7ef2
Ankushm17 <60548952+Ankushm17@users.noreply.github.com>
2020-12-30 19:54:13 +0530

    Add files via upload

---
+++ todosData.js
@@ -0,0 +1,34 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Wake up early",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Chilling",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Mow lawn",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Catch up on the app development",
+        completed: false
+    },
+    {
+        id: 6,
+        text: "Create your own todos app",
+        completed:false
+    }
+]
+
+export default todosData
